211e86c02b57547bd9df71f38afb07fd416a8e76
Edward Facundo <edward.facundo@lds.ifce.edu.br>
2018-06-08 13:02:58 -0300

    add kivy and some archtiecture mods

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+HandGloove
+@author: Edward "Toy" Facundo
+"""
+
+from gui import *
+from server import *
+#import thread
+import time
+
+class Main:
+    print("init")
+    StressCanvasApp().run()
+
+def ___init___():
+    Main()
